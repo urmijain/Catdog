a31d37b522474c35f93abba2def4d3233b8d97a5
urmijain <urmijain1997@gmail.com>
2021-09-22 19:09:01 +0530

    My second commit

--- App1.py
+++ App1.py
@@ -2,6 +2,7 @@ from flask import Flask
 ## WSGI Application This WSGI is Standard which actually used to communicate webserver and web applicaiton to communciate
 app = Flask(__name__) #wsgi applicaiton
 
+
 @app.route("/")
 def welcome():
     return "Welcome Msdhoni Cool"
