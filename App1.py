ddab6351f2e8a3ea5e4479b216f7da1340a58759
urmijain <urmijain1997@gmail.com>
2021-09-22 18:45:28 +0530

    First Commit

---
+++ App1.py
@@ -0,0 +1,19 @@
+from flask import Flask
+## WSGI Application This WSGI is Standard which actually used to communicate webserver and web applicaiton to communciate
+app = Flask(__name__) #wsgi applicaiton
+
+@app.route("/")
+def welcome():
+    return "Welcome Msdhoni Cool"
+
+@app.route("/result/<int:score>")
+def welcome1(score):
+    res=""
+    if score>50:
+        res="pass"
+    else:
+        res="fail"
+    return res
+
+if __name__=='__main__':
+    app.run(debug=True)
